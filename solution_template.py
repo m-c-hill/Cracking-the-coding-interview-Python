eb4703b1939f0c5ae8d24dcd0e0bf2597d7f53e2
m-c-hill <matthewhill2595@gmail.com>
2022-03-27 12:29:16 +0100

    Solution template

---
+++ solution_template.py
@@ -0,0 +1,22 @@
+"""
+Chapter N: Problem M - <Problem name>
+
+<Problem description>
+"""
+
+import unittest
+
+# Solution functions
+def foo():
+    pass
+
+# Tests
+class Test(unittest.TestCase):
+    test_cases = []
+    test_funcs = []
+
+    # Tests funcs here
+
+
+if __name__ == "__main__":
+    unittest.main()
